959fee8d3d8d25d3e8b276cedf77a90715ba1d85
Tobias Jülg <tobias.juelg@tum.de>
2022-03-03 17:52:11 +0100

    updated gitignore, code compiles now

--- source/moepvalidator/main.c
+++ source/moepvalidator/main.c
@@ -101,123 +101,123 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 	int chan_width;
 	long long int freq;
 
-	switch (key) {
-	case 'a':
-		if (!(args->addr = ieee80211_aton(arg)))
-			argp_failure(state, 1, errno, "Invalid hardware address");
-		break;
-	case 'g':
-		if (strlen(arg) != 1)
-			argp_failure(state, 1, errno, "Invalid guard interval: %s", arg);
-		switch (*arg) {
-		case 's':
-			args->gi = IEEE80211_RADIOTAP_MCS_SGI;
-			break;
-		case 'l':
-			args->gi = 0;
-			break;
-		default:
-			argp_failure(state, 1, errno, "Invalid guard interval: %s", arg);
-		}
-		break;
-	case 'h':
-		if (!arg) {
-			args->chan_width = MOEP80211_CHAN_WIDTH_20;
-			break;
-		}
-		chan_width = strtol(arg, &endptr, 0);
-		switch (chan_width) {
-		case 20:
-			args->chan_width = MOEP80211_CHAN_WIDTH_20;
-			break;
-		case 40:
-			args->chan_width = MOEP80211_CHAN_WIDTH_40;
-			if (!endptr)
-				argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
-			switch (*(endptr++)) {
-			case '+':
-				args->freq1 += 10;
-				break;
-			case '-':
-				args->freq1 += 10;
-				break;
-			default:
-				argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
-			}
-			break;
-		default:
-			argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
-		}
-		if (endptr != NULL && endptr != arg + strlen(arg))
-			argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
-		break;
-	case 'i':
-		if (!(inet_aton(arg, &args->ip)))
-			argp_failure(state, 1, errno, "Invalid ip address");
-		break;
-	case 'm':
-		args->mtu = strtol(arg, &endptr, 0);
-		if (endptr != NULL && endptr != arg + strlen(arg))
-			argp_failure(state, 1, errno, "Invalid mtu: %s", arg);
-		if (args->mtu <= 0)
-			argp_failure(state, 1, errno, "Invalid mtu: %d", args->mtu);
-		break;
-	case 'b':
-		args->beacon = strtol(arg, &endptr, 0);
-		if (endptr != NULL && endptr != arg + strlen(arg))
-			argp_failure(state, 1, errno, "Invalid beacon interval: %s", arg);
-		if (args->beacon <= 0)
-			argp_failure(state, 1, errno, "Invalid beacon interval: %d", args->beacon);
-		break;
-	case 'r':
-		args->rate = strtol(arg, &endptr, 0);
-		if (endptr != NULL && endptr != arg + strlen(arg))
-			argp_failure(state, 1, errno, "Invalid data rate: %s", arg);
-		if (args->rate < 0)
-			argp_failure(state, 1, errno, "Invalid data rate: %d", args->rate);
-		break;
-	case ARGP_KEY_ARG:
-		switch (state->arg_num) {
-		case FIX_ARG_IF:
-			args->rad = arg;
-			break;
-		case FIX_ARG_FREQ:
-			freq = strtoll(arg, &endptr, 0);
-			while (endptr != NULL && endptr != arg + strlen(arg)) {
-				switch (*endptr) {
-				case 'k':
-				case 'K':
-					freq *= 1000;
-					break;
-				case 'm':
-				case 'M':
-					freq *= 1000000;
-					break;
-				case 'g':
-				case 'G':
-					freq *= 1000000000;
-					break;
-				default:
-					argp_failure(state, 1, errno, "Invalid frequency: %s", arg);
-				}
-				endptr++;
-			}
-			if (freq < 0)
-				argp_failure(state, 1, errno, "Invalid frequency: %lld", freq);
-			args->freq = freq / 1000000;
-			args->freq1 += args->freq;
-			break;
-		default:
-			argp_usage(state);
-		}
-		break;
-	case ARGP_KEY_END:
-		if (state->arg_num < FIX_ARG_CNT)
-			argp_usage(state);
-		break;
-	default:
-		return ARGP_ERR_UNKNOWN;
-	}
+	// switch (key) {
+	// case 'a':
+	// 	if (!(args->addr = ieee80211_aton(arg)))
+	// 		argp_failure(state, 1, errno, "Invalid hardware address");
+	// 	break;
+	// case 'g':
+	// 	if (strlen(arg) != 1)
+	// 		argp_failure(state, 1, errno, "Invalid guard interval: %s", arg);
+	// 	switch (*arg) {
+	// 	case 's':
+	// 		args->gi = IEEE80211_RADIOTAP_MCS_SGI;
+	// 		break;
+	// 	case 'l':
+	// 		args->gi = 0;
+	// 		break;
+	// 	default:
+	// 		argp_failure(state, 1, errno, "Invalid guard interval: %s", arg);
+	// 	}
+	// 	break;
+	// case 'h':
+	// 	if (!arg) {
+	// 		args->chan_width = MOEP80211_CHAN_WIDTH_20;
+	// 		break;
+	// 	}
+	// 	chan_width = strtol(arg, &endptr, 0);
+	// 	switch (chan_width) {
+	// 	case 20:
+	// 		args->chan_width = MOEP80211_CHAN_WIDTH_20;
+	// 		break;
+	// 	case 40:
+	// 		args->chan_width = MOEP80211_CHAN_WIDTH_40;
+	// 		if (!endptr)
+	// 			argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
+	// 		switch (*(endptr++)) {
+	// 		case '+':
+	// 			args->freq1 += 10;
+	// 			break;
+	// 		case '-':
+	// 			args->freq1 += 10;
+	// 			break;
+	// 		default:
+	// 			argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
+	// 		}
+	// 		break;
+	// 	default:
+	// 		argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
+	// 	}
+	// 	if (endptr != NULL && endptr != arg + strlen(arg))
+	// 		argp_failure(state, 1, errno, "Invalid HT mode: %s", arg);
+	// 	break;
+	// case 'i':
+	// 	if (!(inet_aton(arg, &args->ip)))
+	// 		argp_failure(state, 1, errno, "Invalid ip address");
+	// 	break;
+	// case 'm':
+	// 	args->mtu = strtol(arg, &endptr, 0);
+	// 	if (endptr != NULL && endptr != arg + strlen(arg))
+	// 		argp_failure(state, 1, errno, "Invalid mtu: %s", arg);
+	// 	if (args->mtu <= 0)
+	// 		argp_failure(state, 1, errno, "Invalid mtu: %d", args->mtu);
+	// 	break;
+	// case 'b':
+	// 	args->beacon = strtol(arg, &endptr, 0);
+	// 	if (endptr != NULL && endptr != arg + strlen(arg))
+	// 		argp_failure(state, 1, errno, "Invalid beacon interval: %s", arg);
+	// 	if (args->beacon <= 0)
+	// 		argp_failure(state, 1, errno, "Invalid beacon interval: %d", args->beacon);
+	// 	break;
+	// case 'r':
+	// 	args->rate = strtol(arg, &endptr, 0);
+	// 	if (endptr != NULL && endptr != arg + strlen(arg))
+	// 		argp_failure(state, 1, errno, "Invalid data rate: %s", arg);
+	// 	if (args->rate < 0)
+	// 		argp_failure(state, 1, errno, "Invalid data rate: %d", args->rate);
+	// 	break;
+	// case ARGP_KEY_ARG:
+	// 	switch (state->arg_num) {
+	// 	case FIX_ARG_IF:
+	// 		args->rad = arg;
+	// 		break;
+	// 	case FIX_ARG_FREQ:
+	// 		freq = strtoll(arg, &endptr, 0);
+	// 		while (endptr != NULL && endptr != arg + strlen(arg)) {
+	// 			switch (*endptr) {
+	// 			case 'k':
+	// 			case 'K':
+	// 				freq *= 1000;
+	// 				break;
+	// 			case 'm':
+	// 			case 'M':
+	// 				freq *= 1000000;
+	// 				break;
+	// 			case 'g':
+	// 			case 'G':
+	// 				freq *= 1000000000;
+	// 				break;
+	// 			default:
+	// 				argp_failure(state, 1, errno, "Invalid frequency: %s", arg);
+	// 			}
+	// 			endptr++;
+	// 		}
+	// 		if (freq < 0)
+	// 			argp_failure(state, 1, errno, "Invalid frequency: %lld", freq);
+	// 		args->freq = freq / 1000000;
+	// 		args->freq1 += args->freq;
+	// 		break;
+	// 	default:
+	// 		argp_usage(state);
+	// 	}
+	// 	break;
+	// case ARGP_KEY_END:
+	// 	if (state->arg_num < FIX_ARG_CNT)
+	// 		argp_usage(state);
+	// 	break;
+	// default:
+	// 	return ARGP_ERR_UNKNOWN;
+	// }
 
 	return 0;
 }
