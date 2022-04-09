b6428a856aac1f7f85643e6f5f726c027429e0b4
Tobias Jülg <tobias@juelg.net>
2022-04-09 19:31:27 +0200

    Added licence

--- src/moepvalidator/statistics.h
+++ src/moepvalidator/statistics.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2021,2022		Tobias Juelg <tobias.juelg@tum.de>
+ *				            Ben Riegel <ben.riegel@tum.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * See COPYING for more details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __STATISTICS_H_
 #define __STATISTICS_H_
 
